b317f2b8eea654e69e8120f3caf8a0239c0f98d5
crow <tian2004@outlook.com>
2021-09-22 22:08:04 -0500

    Se agrega una página en js para registrar la compra de un producto

--- js/saludar.js
+++ js/saludar.js
@@ -1,5 +1,17 @@
-alert("Hola mundo segunda forma");
+//alert("Hola mundo segunda forma");
 
 console.log("hola mundo desde consola");
 
-document.write("Hola mundo desde JavaScript");
+document.write("<h1>Hola mundo desde JavaScript</h1>");
+
+// ` Tipo de datos: String, number, boolean
+
+var nombre = `Juan`;
+let correo = 'juan@correo.com';
+const mayor = 18;
+let edad = 29;
+let altura = 1.81;
+let extranjero = true;
+let mensaje = "Hola,\n soy "+nombre+" mi correo es "+correo+", tengo "+edad+" años";
+let mensaje2 = `Hola, soy ${nombre} mi correo es ${correo}, tengo ${edad} años`;
+document.write("<h2>"+mensaje2+"</h2>");
